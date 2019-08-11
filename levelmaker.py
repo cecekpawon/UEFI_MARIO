af55bd2edef8f33018a0612788b558a1f28d3330
cecekpawon <cecekpawon@users.noreply.github.com>
2019-08-11 09:31:44 +0700

    Python 2 levelmaker backport

--- levelmaker.py
+++ levelmaker.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
-import argparse
+import sys, struct, argparse
+
+PY3 = sys.version_info[0] == 3
+
+def to_bytes(n):
+    return struct.pack('<I', n)
 
 def converter(level, binary):
     with open(level, "r") as inputFile, open(binary, "wb") as outputFile:
         data = inputFile.read().splitlines()
-        
-        outputFile.write(len(data).to_bytes(4, "little"))
-        outputFile.write(len(data[0]).to_bytes(4, "little"))
-        
+
+        if PY3:
+            outputFile.write(len(data).to_bytes(4, "little"))
+            outputFile.write(len(data[0]).to_bytes(4, "little"))
+        else:
+            outputFile.write(to_bytes(len(data)))
+            outputFile.write(to_bytes(len(data[0])))
+
         tiles = 0
         coins = 0
         enemies = 0
@@ -16,14 +25,22 @@ def converter(level, binary):
             x = d.upper().count("S") + d.upper().count("M")
             coins += d.upper().count("C")
             enemies += (len(d) - d.upper().count(".") - t - x) // 2
-            tiles += t 
+            tiles += t
 
-        outputFile.write(tiles.to_bytes(4, "little"))
-        outputFile.write(enemies.to_bytes(4, "little"))
-        outputFile.write(coins.to_bytes(4, "little"))
+        if PY3:
+            outputFile.write(tiles.to_bytes(4, "little"))
+            outputFile.write(enemies.to_bytes(4, "little"))
+            outputFile.write(coins.to_bytes(4, "little"))
+        else:
+            outputFile.write(to_bytes(tiles))
+            outputFile.write(to_bytes(enemies))
+            outputFile.write(to_bytes(coins))
 
         for d in data:
-            outputFile.write(bytes(d.upper(), "ascii"))
+            if PY3:
+                outputFile.write(bytes(d.upper(), "ascii"))
+            else:
+                outputFile.write(bytes(d.upper()))
 
 
 if __name__ == "__main__":
